70bb25e47a8023de6c919713ddcee19becba352f
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-04-11 18:52:35 +0000

    Nuevos cambios

--- src/app.js
+++ src/app.js
@@ -7,53 +7,60 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-
-  const getRandomValue = array => {
-    const numRandom = Math.floor(Math.random() * array.length);
-    return array[numRandom];
-  };
-
-  const getCardValue = () => {
-    const cardValue = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Queen", "King"];
-    return getRandomValue(cardValue);
-  };
-
-  const getCardSuit = () => {
-    const listaPalos = ["spade", "club", "heart", "diamond"];
-    return getRandomValue(listaPalos);
-  };
-
-  function setCard(number, cardSuit) {
-    document.querySelector(".numCard").innerHTML = number;
-    switch (cardSuit) {
-      case "spade":
-        document.querySelector(".card").classList.add("spade");
-        const spade = document.querySelector(".spade");
-        spade.querySelectorAll(".cardSuit").innerHTML = "♠";
-        break;
-      case "club":
-        document.querySelector(".card").classList.add("club");
-        const club = document.querySelector(".club");
-        club.querySelectorAll(".cardSuit").innerHTML = "♣";
-        break;
-      case "heart":
-        document.querySelector(".card").classList.add("heart");
-        const heart = document.querySelector(".heart");
-        heart.querySelectorAll(".cardSuit").innerHTML = "♥";
-        break;
-      case "diamond":
-        document.querySelector(".card").classList.add("diamond");
-        const diamond = document.querySelector(".diamond");
-        diamond.querySelectorAll(".cardSuit").innerHTML = "♦";
-        break;
-    }
-  }
-
-  const play = () => {
-    const cardNumber = getCardValue();
-    const cardSuit = getCardSuit();
-    setCard(cardNumber, cardSuit);
-  };
-
   play();
 };
+
+const getRandomValue = array => {
+  const numRandom = Math.floor(Math.random() * array.length);
+  return array[numRandom];
+};
+
+const getCardValue = () => {
+  const cardValue = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Q", "K"];
+  return getRandomValue(cardValue);
+};
+
+const getCardSuit = () => {
+  const listaPalos = ["spade", "club", "heart", "diamond"];
+  return getRandomValue(listaPalos);
+};
+
+function setCard(number, cardSuit) {
+  document.querySelector(".numCard").innerHTML = number;
+  switch (cardSuit) {
+    case "spade":
+      document.querySelector(".card").classList.add("spade");
+      document.querySelector("#suitUp").innerHTML = "♠";
+      document.querySelector("#suitDown").innerHTML = "♠";
+      break;
+    case "club":
+      document.querySelector(".card").classList.add("club");
+      document.querySelector("#suitUp").innerHTML = "♣";
+      document.querySelector("#suitDown").innerHTML = "♣";
+      break;
+    case "heart":
+      document.querySelector(".card").classList.add("heart");
+      document.querySelector("#suitUp").innerHTML = "♥";
+      document.querySelector("#suitDown").innerHTML = "♥";
+      break;
+    case "diamond":
+      document.querySelector(".card").classList.add("diamond");
+      document.querySelector("#suitUp").innerHTML = "♦";
+      document.querySelector("#suitDown").innerHTML = "♦";
+      break;
+  }
+}
+
+const play = () => {
+  console.log("play");
+  const cardNumber = getCardValue();
+  const cardSuit = getCardSuit();
+  setCard(cardNumber, cardSuit);
+};
+document.querySelector("#btnPlay").addEventListener("click", () => {
+  play();
+});
+
+document.querySelector("#btnInterval").addEventListener("click", () => {
+  setInterval(play(), 1000);
+});
